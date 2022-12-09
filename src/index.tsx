2640827dd8d5db1523d3952e41e85ce20871dece
devcusn <halilibrahimtunc00@gmail.com>
2022-12-09 19:05:34 +0300

    landing page design

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import App from "./pages/landing/Landing";
 import reportWebVitals from "./reportWebVitals";
+
+import "./assets/global.css";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
@@ -12,10 +13,6 @@ import "@fontsource/roboto/700.css";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 reportWebVitals();
---
+++ src/pages/landing/Landing.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { Box, styled, Grid, Typography, Button } from "@mui/material";
+import image from "../../assets/img/background.webp";
+
+const LandingPage: React.FunctionComponent = () => {
+  return (
+    <Grid sx={{ backgroundColor: "rgba(0,0,0,0.5)", minHeight: "100vh" }}>
+      <Grid
+        container
+        direction='row'
+        alignItems='center'
+        sx={{
+          color: "#fafafa",
+          padding: "20px 100px",
+        }}
+      >
+        <Grid sx={{ flex: 1 }}>
+          <Typography color='red' variant='h3' fontWeight='bold'>
+            MUVI
+          </Typography>
+        </Grid>
+        <Box>
+          <Button sx={{ fontSize: "23px", color: "red", width: "150px" }}>
+            Login
+          </Button>
+        </Box>
+      </Grid>
+      <Grid
+        container
+        direction='column'
+        alignItems='center'
+        justifyContent='center'
+        width='100%'
+        height='70vh'
+      >
+        <Box
+          sx={{
+            color: "#fafafa",
+            fontSize: "70px",
+            fontWeight: "bold",
+          }}
+        >
+          MUVI
+        </Box>
+        <Box sx={{ color: "#fafafa", fontSize: "40px" }}>
+          You will find all the latest and greatest films
+        </Box>
+        <Box sx={{ fontSize: "40px", marginTop: "30px" }}>
+          <Button sx={{ color: "red", fontSize: "25px", fontWeight: "bold" }}>
+            Family{" "}
+          </Button>
+          <Button sx={{ color: "red", fontSize: "25px", fontWeight: "bold" }}>
+            Student{" "}
+          </Button>
+          <Button sx={{ color: "red", fontSize: "25px", fontWeight: "bold" }}>
+            Account{" "}
+          </Button>
+        </Box>
+      </Grid>
+      <Grid
+        sx={{
+          position: "fixed",
+          zIndex: -1,
+          top: "0px",
+        }}
+      >
+        <img src={image} className='App-logo' alt='logo' width='100%' />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default LandingPage;
