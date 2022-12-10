b1652e4509467bddd6293e9f67bd6f4aaf3a0ad2
devcusn <halilibrahimtunc00@gmail.com>
2022-12-10 19:57:17 +0300

    feat/not-found-page designed

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Box, Button } from "@mui/material";
 
 const NotFoundPage: React.FunctionComponent = () => {
-  return <div>404 Not Found</div>;
+  const navigate = useNavigate();
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        width: "100%",
+        height: "100vh",
+        backgroundColor: "#101010",
+        color: "red",
+      }}
+    >
+      <Box sx={{ fontSize: "100px", color: "red" }}>404</Box>
+      <Box>Page Not Found</Box>
+      <Button
+        sx={{ color: "red" }}
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Go To Home
+      </Button>
+    </Box>
+  );
 };
 
 export default NotFoundPage;
