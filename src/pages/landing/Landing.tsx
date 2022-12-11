6c4c0b883b05c5bf8937a406134ac53d34fe416e
devcusn <halilibrahimtunc00@gmail.com>
2022-12-11 03:05:40 +0300

    feat/responsive: LandingPage and MembershipPage

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Box, Grid, Typography, Button, styled } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Typography,
+  Button,
+  styled,
+  useMediaQuery,
+} from "@mui/material";
 
 import image from "../../assets/img/background.png";
 import Layout from "../../layout/Layout";
@@ -12,6 +19,7 @@ const Background = styled("div")({
 
 const LandingPage: React.FunctionComponent = () => {
   const navigate = useNavigate();
+  const matches = useMediaQuery("(min-width:800px)", { noSsr: true });
 
   return (
     <Layout pageType='landing'>
@@ -27,10 +35,19 @@ const LandingPage: React.FunctionComponent = () => {
             height: "100vh",
           }}
         >
-          <Typography variant='h1' sx={{ color: "red", fontWeight: "bold" }}>
+          <Typography
+            variant={matches ? "h1" : "h4"}
+            sx={{ color: "red", fontWeight: "bold" }}
+          >
             MovieUVI
           </Typography>
-          <Box sx={{ color: "#fafafa", fontSize: "40px" }}>
+          <Box
+            sx={{
+              color: "#fafafa",
+              fontSize: matches ? "40px" : "20px",
+              textAlign: "center",
+            }}
+          >
             You will find all the latest and greatest films
           </Box>
           <Box sx={{ marginTop: "30px" }}>
@@ -38,7 +55,7 @@ const LandingPage: React.FunctionComponent = () => {
               variant='contained'
               sx={{
                 color: "#fafafa",
-                fontSize: "25px",
+                fontSize: matches ? "25px" : "15px",
                 fontWeight: "bold",
                 backgroundColor: "red",
               }}
